ff7d30932923815d465a5df12776d99295b0826a
Enrique Perez Soler <quiqueperezsoler@gmail.com>
2018-08-21 12:18:35 +0200

    Importing the code refactored and working

---
+++ public_html/modal.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Modal{
+	constructor(meshData){
+		this.transform = new Transform();
+		this.mesh = meshData;
+	}
+
+	//--------------------------------------------------------------------------
+	//Getters/Setters
+	setScale(x,y,z){ this.transform.scale.set(x,y,z); return this; }
+	setPosition(x,y,z){ this.transform.position.set(x,y,z); return this; }
+	setRotation(x,y,z){ this.transform.rotation.set(x,y,z); return this; }
+
+	addScale(x,y,z){	this.transform.scale.x += x;	this.transform.scale.y += y;	this.transform.scale.y += y;	return this; }
+	addPosition(x,y,z){	this.transform.position.x += x; this.transform.position.y += y; this.transform.position.z += z; return this; }
+	addRotation(x,y,z){	this.transform.rotation.x += x; this.transform.rotation.y += y; this.transform.rotation.z += z; return this; }
+
+	//--------------------------------------------------------------------------
+	//Things to do before its time to render
+	preRender(){ this.transform.updateMatrix(); return this; }
+}
